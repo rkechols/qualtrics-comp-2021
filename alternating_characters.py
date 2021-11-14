dc437548104c4be229e561d1e5a3e2d11985dcbc
Ryan Echols <rkechols21@gmail.com>
2021-11-13 18:48:51 -0700

    added in-browswer type solution to alternating characters

---
+++ alternating_characters.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import os
+
+
+# <code>
+
+
+def alternatingCharacters(s):
+    dels = 0
+    n = len(s)
+    for i in range(1, n):
+        if s[i] == s[i - 1]:
+            dels += 1
+    return dels
+
+
+# </code>
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    q = int(input().strip())
+    for q_itr in range(q):
+        s = input()
+        result = alternatingCharacters(s)
+        fptr.write(str(result) + '\n')
+    fptr.close()
