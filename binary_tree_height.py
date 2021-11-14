60ec7c4091d0f8799bc965e6845ff58a26915794
Ryan Echols <rkechols21@gmail.com>
2021-11-13 18:42:25 -0700

    added in-browser type solution to binary tree height

---
+++ binary_tree_height.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def create(self, val):
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+# <code>
+
+
+def height(root) -> int:
+    if root is None:
+        return -1
+    return 1 + max(height(root.left), height(root.right))
+
+
+# </code>
+
+
+tree = BinarySearchTree()
+t = int(input())
+arr = list(map(int, input().split()))
+for i in range(t):
+    tree.create(arr[i])
+print(height(tree.root))
+
